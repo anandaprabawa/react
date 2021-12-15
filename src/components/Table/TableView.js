68d91449058eba30d3550dbf1d3dcff51ccfde85
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2021-12-15 16:49:58 +0800

    feat: sort product date

--- src/components/Table/TableView.js
+++ src/components/Table/TableView.js
@@ -5,7 +5,7 @@ import { format as formatDate } from "date-fns";
 export const TableView = ({ products }) => {
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "name", headerName: "Name", width: 130 },
+    { field: "name", headerName: "Name", width: 250 },
     {
       field: "price",
       headerName: "Price",
@@ -20,12 +20,19 @@ export const TableView = ({ products }) => {
       field: "createdAt",
       headerName: "Created Date",
       width: 200,
-      valueGetter: (params) => {
+      valueFormatter: (params) => {
         const date = new Date(params.value);
         return formatDate(date, "dd MMMM yyyy");
       },
     },
   ];
+
+  const [sortModel] = React.useState([
+    {
+      field: "createdAt",
+      sort: "desc",
+    },
+  ]);
 
   return (
     <div style={{ height: 400, width: "100%" }}>
@@ -34,7 +41,7 @@ export const TableView = ({ products }) => {
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        checkboxSelection
+        sortModel={sortModel}
       />
     </div>
   );
